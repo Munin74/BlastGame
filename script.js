d5ac0411e0c6897e7b3a321cd1513506dbe5e4ee
Munin74 <73397612+Munin74@users.noreply.github.com>
2020-11-02 00:25:11 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,58 +4,72 @@ class Board {
         this.cells=[];
         this.height=h;
         this.width=w;
+        this.ClickBoard=this.OnClickBoard.bind(this);
+        this.gameBoard=document.querySelector('.game-board');
     }
 
-    OnClickBoard(event){
-        
+    OnClickBoard(ball){
+        this.RemoveLeftBalls(ball);
+        this.RemoveRightBalls(ball);
+        this.cells[ball.y][ball.x].cell.classList.remove(ball.color);
     };
 
     Init(){
         for (let y=0; y<this.height; y++){
             this.cells[y]=[]
             for(let x=0; x<this.width; x++) {
-                let gameBoard=document.querySelector('.game-board');
                 let ball=new Ball(x,y);
                 ball.Init();
                 this.cells[y][x]=ball;
-                ball.OnClick=this.OnClickBoard;
-                gameBoard.appendChild(ball.cell);
+                ball.OnClick=this.ClickBoard;
+                this.gameBoard.appendChild(ball.cell);
             }
         }
     }
-    GetRemoveCells() {
-       
+    RemoveLeftBalls(ball)
+    {
+        while(ball.x>=0 & this.cells[ball.y][ball.x-1].color==ball.color)
+        {
+            this.cells[ball.y][ball.x-1].cell.classList.remove(ball.color);
+            ball.x--;
+            
+        }
     }
-    IsFinished(){
 
+    RemoveRightBalls(ball)
+    {
+        let checkX=ball.x;
+        let width=this.width;
+        while(checkX<width-1 & this.cells[ball.y][checkX].color==ball.color) 
+        {
+            this.cells[ball.y][checkX+1].cell.classList.remove(ball.color);
+            checkX++;
+        }
     }
-
 };
 
 class Ball {
     constructor(x, y) {
-        this.X=x;
-        this.Y=y;
-        this.cell;
-        this.color;
+        this.x=x;
+        this.y=y;
     }
 
     Init(){
         let sender = this;
         this.cell=document.createElement('div');
         this.cell.classList.add('tile');
-        this.SetColor();
+        this.SetColor(GetColor());
         this.cell.addEventListener('click',function(event) {
             sender.OnClick(sender);
         });
     }
 
     OnClick(event){
-
+        
     };
 
-    SetColor() {
-        this.color=GetColor();
+    SetColor(color) {
+        this.color=color;
         this.cell.classList.add(this.color);
     }
 };
@@ -65,9 +79,6 @@ function GetColor(){
     return Color[Math.floor(Math.random() * Color.length)];
 }
 
-let gameBoard=document.querySelector('.game-board');
-
 let board=new Board(9,9);
 
 board.Init();
-
